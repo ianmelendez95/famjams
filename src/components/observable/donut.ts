6c362769efc7682984aa2b9b49acee46a9e2fba3
Ian Melendez <imelendez@preludedynamics.com>
2023-03-11 13:22:56 -0600

    [d3] donut chart fully typed

--- src/components/observable/donut.ts
+++ src/components/observable/donut.ts
@@ -1,21 +1,23 @@
 import * as d3 from 'd3'
+import type {BaseType, DefaultArcObject, PieArcDatum} from "d3";
 
-export function multiply(a: number, { x = 2, y }: {x?: number, y?: number} = {}) {
-    return a * x
+interface DonutChartData {
+    name: string,
+    value: number
 }
 
 interface DonutChartOptions {
-    name?: (xs: [string, number]) => string,  // given d in data, returns the (ordinal) label
-    value?: (xs: [string, number]) => number, // given d in data, returns the (quantitative) value
-    title?: string, // given d in data, returns the title text
+    name?: (xs: DonutChartData) => string,  // given d in data, returns the (ordinal) label
+    value?: (xs: DonutChartData) => number, // given d in data, returns the (quantitative) value
+    title?: (d: DonutChartData | number, i?: number, data?: DonutChartData[]) => string, // given d in data, returns the title text
     width?: number, // outer width, in pixels
     height?: number, // outer height, in pixels
     innerRadius?: number, // inner radius of pie, in pixels (non-zero for donut)
     outerRadius?: number, // outer radius of pie, in pixels
     labelRadius?: number, // center radius of labels
     format?: string, // a format specifier for values (in the label)
-    names?: string[], // array of names (the domain of the color scale)
-    colors?: string[], // array of colors for names
+    names?: string[] | Set<string>, // array of names (the domain of the color scale)
+    colors?: readonly string[], // array of colors for names
     stroke?: string, // stroke separating widths
     strokeWidth?: number, // width of stroke separating wedges
     strokeLinejoin?: string, // line join of stroke separating wedges
@@ -25,9 +27,9 @@ interface DonutChartOptions {
 // Copyright 2021 Observable, Inc.
 // Released under the ISC license.
 // https://observablehq.com/@d3/donut-chart
-export function DonutChart(data: any, {
-    name = ([x]) => x,  // given d in data, returns the (ordinal) label
-    value = ([, y]) => y, // given d in data, returns the (quantitative) value
+export function DonutChart(data: DonutChartData[], {
+    name,  // given d in data, returns the (ordinal) label
+    value, // given d in data, returns the (quantitative) value
     title, // given d in data, returns the title text
     width = 640, // outer width, in pixels
     height = 400, // outer height, in pixels
@@ -43,17 +45,17 @@ export function DonutChart(data: any, {
     padAngle = stroke === "none" ? 1 / outerRadius : 0, // angular separation between wedges
   }: DonutChartOptions = {}) {
     // Compute values.
-    const N = d3.map(data, name);
-    const V = d3.map(data, value);
-    const I = d3.range(N.length).filter(i => !isNaN(V[i]));
+    const N: string[] = d3.map(data, name!);
+    const V = d3.map(data, value!);
+    const I = d3.range(N.length).filter((i: number) => !isNaN(V[i]));
 
     // Unique the names.
     if (names === undefined) names = N;
-    names = new d3.InternSet(names);
+    names = new d3.InternSet(names) as Set<string>;
 
     // Chose a default color scheme based on cardinality.
     if (colors === undefined) colors = d3.schemeSpectral[names.size];
-    if (colors === undefined) colors = d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), names.size);
+    if (colors === undefined) colors = d3.quantize((t: number) => d3.interpolateSpectral(t * 0.8 + 0.1), names.size);
 
     // Construct scales.
     const color = d3.scaleOrdinal(names, colors);
@@ -61,15 +63,15 @@ export function DonutChart(data: any, {
     // Compute titles.
     if (title === undefined) {
       const formatValue = d3.format(format);
-      title = i => `${N[i]}\n${formatValue(V[i])}`;
+      title = (i: number | DonutChartData) => `${N[i as number]}\n${formatValue(V[i as number])}`;
     } else {
-      const O = d3.map(data, d => d);
+      const O: DonutChartData[] = d3.map(data, (d: DonutChartData) => d);
       const T = title;
-      title = i => T(O[i], i, data);
+      title = (i: number | DonutChartData) => T(O[i as number], i as number, data);
     }
 
     // Construct arcs.
-    const arcs = d3.pie().padAngle(padAngle).sort(null).value(i => V[i])(I);
+    const arcs = d3.pie().padAngle(padAngle).sort(null).value((i) => V[i as number])(I);
     const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
     const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
 
@@ -86,10 +88,10 @@ export function DonutChart(data: any, {
       .selectAll("path")
       .data(arcs)
       .join("path")
-      .attr("fill", d => color(N[d.data]))
-      .attr("d", arc)
+      .attr("fill", (d: PieArcDatum<any>) => color(N[d.data]))
+      .attr("d", arc as any)
       .append("title")
-      .text(d => title(d.data));
+      .text((d: PieArcDatum<any>) => title!(d.data));
 
     svg.append("g")
       .attr("font-family", "sans-serif")
@@ -98,17 +100,17 @@ export function DonutChart(data: any, {
       .selectAll("text")
       .data(arcs)
       .join("text")
-      .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+      .attr("transform", (d: unknown) => `translate(${arcLabel.centroid(d as DefaultArcObject)})`)
       .selectAll("tspan")
-      .data(d => {
-        const lines = `${title(d.data)}`.split(/\n/);
+      .data((d: PieArcDatum<any>) => {
+        const lines = `${title!(d.data)}`.split(/\n/);
         return (d.endAngle - d.startAngle) > 0.25 ? lines : lines.slice(0, 1);
       })
       .join("tspan")
       .attr("x", 0)
-      .attr("y", (_, i) => `${i * 1.1}em`)
-      .attr("font-weight", (_, i) => i ? null : "bold")
+      .attr("y", (_: string, i: number) => `${i * 1.1}em`)
+      .attr("font-weight", (_: string, i: number) => i ? null : "bold")
       .text(d => d);
 
-    return Object.assign(svg.node(), {scales: {color}});
+    return Object.assign(svg.node() as SVGSVGElement, {scales: {color}});
   }
