72f4d47ba58b7d5e1113ba64b00c6e84e27fd679
Ian Melendez <imelendez@preludedynamics.com>
2023-03-12 15:51:55 -0500

    [playlist] now return all track information from api method

--- src/components/playlist/PlaylistStats.vue
+++ src/components/playlist/PlaylistStats.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
 // import type {UserProfile} from '@/spotify/types'
-import type {UserProfile} from "@/spotify/types";
-import {getUsersToTrackCount} from "@/famjams/playlist";
+import type {Track, UserProfile} from "@/spotify/types";
+import {getUsersToTracks} from "@/famjams/playlist";
 import {ref, onMounted} from "vue";
 import {DonutChart} from "@/components/observable/donut";
 
 const accessToken = localStorage.getItem("accessToken") as string
 const playlistId = localStorage.getItem("playlistId") as string
 
-const userTrackCounts: Map<UserProfile, number> = await getUsersToTrackCount(accessToken, playlistId)
+const userTracks: Map<UserProfile, Track[]> = await getUsersToTracks(accessToken, playlistId)
+const userTrackCounts: [UserProfile, number][] = [...userTracks.entries()].map(([u, ts]) => [u, ts.length])
 
 let d3DonutDiv = ref<HTMLDivElement | null>(null)
 
-const chartData = [...userTrackCounts.entries()]
+const chartData = userTrackCounts
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     .sort(([_1, c1], [_2, c2]) => c2 - c1)
-    .map(([profile, count]) => ({ user: profile, value: count }))
+    .map(([user, count]) => ({ user, value: count }))
 
 const d3DonutSvg = DonutChart(chartData)
 
--- src/famjams/playlist.ts
+++ src/famjams/playlist.ts
@@ -1,12 +1,12 @@
 import {fetchUserTracks} from "@/famjams/tracks";
 import {fetchUserProfile} from "@/spotify/api";
-import type {UserProfile} from "@/spotify/types";
+import type {Track, UserProfile} from "@/spotify/types";
 
-export async function getUsersToTrackCount(accessToken: string, playlistId: string): Promise<Map<UserProfile, number>> {
+export async function getUsersToTracks(accessToken: string, playlistId: string): Promise<Map<UserProfile, Track[]>> {
     return new Map(await Promise.all([...(await fetchUserTracks(accessToken, playlistId)).entries()]
         .map(async ([userId, tracks]) => {
             const profile = await fetchUserProfile(accessToken, userId)
-            return ([profile, tracks.length] as [UserProfile, number])
+            return ([profile, tracks] as [UserProfile, Track[]])
         })
     ))
 }
