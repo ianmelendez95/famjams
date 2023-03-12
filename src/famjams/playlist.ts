2cac4a6dd22c42f5cef44463a40e4665f09b93cd
Ian Melendez <imelendez@preludedynamics.com>
2023-03-11 18:47:17 -0600

    [playlist] one-lined getUsernamesToTrackCount
    - Because I could, without asking whether or not I should

--- src/famjams/playlist.ts
+++ src/famjams/playlist.ts
@@ -2,14 +2,10 @@ import {fetchUserTracks} from "@/famjams/tracks";
 import {fetchUserProfile} from "@/spotify/api";
 
 export async function getUsernamesToTrackCount(accessToken: string, playlistId: string): Promise<Map<string, number>> {
-    const userTracks: Map<string, Track[]> = await fetchUserTracks(accessToken, playlistId)
-
-    const usernameCounts = new Map<string, number>()
-    for (const [userId, tracks] of userTracks) {
-        const username = (await fetchUserProfile(accessToken, userId)).display_name
-
-        usernameCounts.set(username, tracks.length)
-    }
-
-    return usernameCounts
+    return new Map(await Promise.all([...(await fetchUserTracks(accessToken, playlistId)).entries()]
+        .map(async ([userId, tracks]) => {
+            const {display_name} = await fetchUserProfile(accessToken, userId)
+            return ([display_name, tracks.length] as [string, number])
+        })
+    ))
 }
