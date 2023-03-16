460c840233be2e128adb6a36c073509dea921189
Ian Melendez <imelendez@preludedynamics.com>
2023-03-16 18:36:39 -0500

    [select] images with (slightly) better styling

--- src/components/playlist/PlaylistSelect.vue
+++ src/components/playlist/PlaylistSelect.vue
@@ -19,9 +19,14 @@ function selectPlaylist(playlistId: string) {
 
 <template>
   <div>
-    <div v-for="playlist in playlists" :key="playlist.id" @click="selectPlaylist(playlist.id)">
-      {{ playlist.name }}
-      <hr/>
+    <div v-for="playlist in playlists"
+         :key="playlist.id"
+         @click="selectPlaylist(playlist.id)"
+         class="flex-row p-4 border-b border-slate-500 cursor-pointer">
+      <div class="inline-block pr-4">
+        <img v-if="playlist.images[0]" v-bind:src="playlist.images[0].url" class="w-16" alt="[Playlist Image]"/>
+      </div>
+      <div class="inline-block">{{ playlist.name }}</div>
     </div>
   </div>
 </template>
