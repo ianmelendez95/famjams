393bb55214b2feaf64f8a10d5c4c59ab92253ed5
Ian Melendez <imelendez@preludedynamics.com>
2023-03-20 20:41:15 -0500

    [stats] initial 'quantity' award category

--- src/components/playlist/PlaylistStats.vue
+++ src/components/playlist/PlaylistStats.vue
@@ -29,14 +29,37 @@ const chartData = userTrackCounts
 onMounted(() => {
   const donutDiv = donutDivRef.value!
   const width = donutDiv.getBoundingClientRect().width
-  replaceDivBody(donutDiv, buildDonut(chartData, width))
+  donutDiv.append(buildDonut(chartData, width))
+  // replaceDivBody(donutDiv, buildDonut(chartData, width))
 })
 </script>
 
 <template>
-  <div class="flex flex-row justify-center">
-    <div ref="donutDivRef"
-         class="max-w-full w-[320px]"></div>
+  <div class="flex flex-col items-center">
+    <div class="pb-4">
+      <h1 class="font-bold text-5xl text-center text-slate-300 pb-4">
+        Quantity <span class="font-bold" style="text-decoration: underline">is</span> Quality
+      </h1>
+      <p class="text-base text-center text-slate-400">
+        Number of Tracks Contributed
+      </p>
+    </div>
+    <div class="flex flex-row justify-center pb-6">
+      <div ref="donutDivRef" class="max-w-full w-[320px]"></div>
+    </div>
+    <div class="p-4 border-slate-400 border-2 rounded-lg text-slate-400">
+      <table>
+        <tr v-for="([user, count], i) in userTrackCounts.slice(0, 3)"
+            class="text-2xl"
+            :class="{
+              'text-slate-200': ((i === 0) || (userTrackCounts[i][1] === userTrackCounts[0][1])),
+              'text-2xl': ((i === 0) || (userTrackCounts[i][1] === userTrackCounts[0][1]))
+            }">
+          <td><div class="pr-4">{{ count }}</div></td>
+          <td>{{ user.display_name }}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
