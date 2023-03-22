92f93b1d07d619152fba46b15e7f652f475e9550
Ian Melendez <imelendez@preludedynamics.com>
2023-03-22 00:43:04 -0500

    [auth] Spotify auth done with current url for redirect
    - Technically a slight vulnerability, but is far more convenient than setting during the build, and this is a hobby project after all.

--- src/spotify/authCodeWithPkce.ts
+++ src/spotify/authCodeWithPkce.ts
@@ -3,8 +3,6 @@
 import {getClientId} from "@/famjams/constants";
 import hash from 'hash.js'
 
-const REDIRECT_URI = "http://192.168.1.116:5173/callback"
-
 export async function redirectToAuthCodeFlow() {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
@@ -14,7 +12,7 @@ export async function redirectToAuthCodeFlow() {
     const params = new URLSearchParams();
     params.append("client_id", getClientId());
     params.append("response_type", "code");
-    params.append("redirect_uri", REDIRECT_URI);
+    params.append("redirect_uri", document.location.origin + '/callback');
     // params.append("scope", "user-read-private user-read-email");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -29,7 +27,7 @@ export async function getAccessToken(code: string) {
     params.append("client_id", getClientId());
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", REDIRECT_URI);
+    params.append("redirect_uri", document.location.origin + '/callback');
     params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
