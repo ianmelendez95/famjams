cd913215bdbc9447ba2836c0e8b4376523082f5d
Ian Melendez <imelendez@preludedynamics.com>
2023-03-28 17:12:59 -0500

    [quantity] initial large screen layout

--- src/components/playlist/stat/PlaylistQuantity.vue
+++ src/components/playlist/stat/PlaylistQuantity.vue
@@ -19,7 +19,7 @@ onMounted(() => {
 
 <template>
   <div class="flex flex-col items-center">
-    <div class="pb-4">
+    <div class="pb-10">
       <h1 class="font-bold text-5xl text-center text-slate-300 pb-4">
         {{ t('playlistStats.quantity.title1') }}
         <span class="font-bold" style="text-decoration: underline">
@@ -31,22 +31,24 @@ onMounted(() => {
         {{ t("playlistStats.quantity.subtitle") }}
       </p>
     </div>
-    <div class="flex flex-row justify-center pb-6">
-      <div ref="donutDivRef_count" class="max-w-full w-[320px]"></div>
-    </div>
-    <div class="p-4 border-slate-400 border-2 rounded-lg text-slate-400">
-      <table>
-        <tr v-for="([user, count], i) in props.counts.slice(0, 3)"
-            class="text-2xl"
-            :class="{
+    <div class="flex flex-row items-center justify-center">
+      <div class="flex flex-row justify-center pb-6 pr-6">
+        <div ref="donutDivRef_count" class="max-w-full w-[320px]"></div>
+      </div>
+      <div class="p-4 border-slate-400 border-2 rounded-lg text-slate-400">
+        <table>
+          <tr v-for="([user, count], i) in props.counts.slice(0, 3)"
+              class="text-2xl"
+              :class="{
               'text-slate-200': ((i === 0) || (props.counts[i][1] === props.counts[0][1])),
               'text-2xl': ((i === 0) || (props.counts[i][1] === props.counts[0][1]))
             }"
-            :key="user.id">
-          <td><div class="pr-4">{{ count }}</div></td>
-          <td>{{ user.display_name }}</td>
-        </tr>
-      </table>
+              :key="user.id">
+            <td><div class="pr-4">{{ count }}</div></td>
+            <td>{{ user.display_name }}</td>
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
 </template>
