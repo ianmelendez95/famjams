d3c74690ae23c9b5eb059ee7249fdeb3050c5a88
Ian Melendez <imelendez@preludedynamics.com>
2023-03-28 17:32:44 -0500

    [popularity] update to be responsive as well

--- src/components/playlist/stat/PlaylistPopularity.vue
+++ src/components/playlist/stat/PlaylistPopularity.vue
@@ -19,7 +19,7 @@ onMounted(() => {
 
 <template>
   <div class="flex flex-col items-center">
-    <div class="pb-4">
+    <div class="pb-10">
       <h1 class="font-bold text-5xl text-center text-slate-300 pb-4">
         {{ t('playlistStats.popularity.title') }}
       </h1>
@@ -27,22 +27,24 @@ onMounted(() => {
         {{ t("playlistStats.popularity.subtitle") }}
       </p>
     </div>
-    <div class="flex flex-row justify-center pb-6">
-      <div ref="donutDivRef" class="max-w-full w-[320px]"></div>
-    </div>
-    <div class="p-4 border-slate-400 border-2 rounded-lg text-slate-400">
-      <table>
-        <tr v-for="([user, avg], i) in props.averages.slice(0, 3)"
-            class="text-2xl"
-            :class="{
+    <div class="md:flex flex-row items-center justify-center">
+      <div class="flex flex-row justify-center pb-6 md:pr-6">
+        <div ref="donutDivRef" class="max-w-full w-[320px]"></div>
+      </div>
+      <div class="p-4 border-slate-400 border-2 rounded-lg text-slate-400">
+        <table>
+          <tr v-for="([user, avg], i) in props.averages.slice(0, 3)"
+              class="text-2xl"
+              :class="{
               'text-slate-200': ((i === 0) || (props.averages[i][1] === props.averages[0][1])),
               'text-2xl': ((i === 0) || (props.averages[i][1] === props.averages[0][1]))
             }"
-            :key="user.id">
-          <td><div class="pr-4">{{ avg.toFixed(2) }}</div></td>
-          <td>{{ user.display_name }}</td>
-        </tr>
-      </table>
+              :key="user.id">
+            <td><div class="pr-4">{{ avg.toFixed(2) }}</div></td>
+            <td>{{ user.display_name }}</td>
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
 </template>
