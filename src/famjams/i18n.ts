869aa4441a8301ba41d9c836bc01621ce0cabcb0
Ian Melendez <imelendez@preludedynamics.com>
2023-04-07 14:40:25 -0500

    [template] fix i18n symbol escape

--- src/famjams/i18n.ts
+++ src/famjams/i18n.ts
@@ -27,7 +27,7 @@ export const messages = {
         },
         enroll: {
             p1: "Your user has not been added to the development access list.",
-            p2: "If you're interested in using the app please email ian.melendez.95@gmail.com with your full name and email associated with your Spotify account.",
+            p2: "If you're interested in using the app please email ian.melendez.95{'@'}gmail.com with your full name and email associated with your Spotify account.",
             p3: "This is really just for friends and family. I just wanted this to sound like generic marketing material because that looks cool."
         },
         playlistSelect: {
